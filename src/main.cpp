7733dadd6c7d78779713ee876c23de512bbb0311
Brian James <github@tofof.com>
2024-06-15 15:34:10 -0500

    testing distances

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ static Button buttonC(JOY_C, buttonHandler);
 #define STEP_PIN  16     // Step
 #define ENDWARD true
 #define MOTORWARD false
+bool direction = ENDWARD;
 bool shouldStep = false;
 void doStep();
 
@@ -66,6 +67,28 @@ static void buttonHandler(uint8_t btnId, uint8_t btnState) {
       digitalWrite(DIR_PIN, btnId == JOY_L ? ENDWARD : MOTORWARD);
       shouldStep = true;
       Serial.print("shouldStep"); Serial.println(shouldStep);
+    }
+    if (btnId == JOY_C) {                                     //ms 12- (there is no ms3 on a tmc2208 so this dip should do nothing?)
+      const int steps = 48000;                                //   00: 20000 -> 62.47 millimeters, or .0031235 mm/step, or 160 steps per 0.5mm thread at 1/8 so 20 full steps per rev
+      Serial.print("Stepping "); Serial.println(steps);       //   11: 40000 -> 62.91 millimeters, or .00157275 mm/step, or 318 steps per 0.5mm thread at 1/16 so 19.9 full steps per rev
+      digitalWrite(DIR_PIN, direction);                       //   11: 48000 -> 75.60 millimeters, or .001575 mm/step, or 317.4 steps per 0.5mm thread
+      delay(10);
+      for (int i = 0; i<steps; i++) {
+        if (i%1000==0) Serial.println(i);
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(40);
+        digitalWrite(STEP_PIN, LOW);
+        delayMicroseconds(40);
+      }
+      Serial.print("Done stepping "); Serial.println(steps);
+    }
+    if (btnId == JOY_U) { 
+      direction = MOTORWARD;
+      Serial.println("Setting direction to motorward");
+    }
+    if (btnId == JOY_D) { 
+      direction = ENDWARD;
+      Serial.println("Setting direction to endward");
     }
   } else {
     // btnState == BTN_OPEN.
